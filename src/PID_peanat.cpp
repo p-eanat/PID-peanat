f05bf280da8bd4066d56abf406824a8f8e1854fd
unknown <amyinventions@gmail.com>
2023-04-03 22:45:48 -0700

    Cleaned and updated code

--- src/PID_peanat.cpp
+++ src/PID_peanat.cpp
@@ -1,26 +1,35 @@
 /* 
  * Peanat's PID library made for Jeremy the robot, but should work well for all other robot needs too!
- * Notable features: custom initial output, reverse mode, integral windup prevention, deadband limits
+ * Extra features: custom initial output, reverse mode, integral windup prevention, deadband limits
  * Date created: Aug. 16, 2022
- * Version: 1.0
- * Date modified: Aug. 18, 2022
  */
 
 #include "Arduino.h"
-#include "PID_peanut.h"
+#include "PID_peanat.h"
 
-PID::PID(double user_kp, double user_ki, double user_kd, double user_init_output, bool user_reverse, unsigned long user_sample_time, double user_min_output, double user_max_output, double user_deadband) {
+const double SECS_IN_MS = 0.001;
+
+
+PID::PID(double user_kp, double user_ki, double user_kd) {
+    /* 
+     * Initialize PID object.
+     *
+     * @param user_kp: proportional coefficient, should be >= 0
+     * @param user_ki: integral coefficient, should be >= 0
+     * @param user_kd: derivative coefficient, should be >= 0
+     * 
+     * @returns PID object
+     */
+
     kp = user_kp;
     ki = user_ki;
     kd = user_kd;
-    init_output = user_init_output;
-    reverse = user_reverse;
-    sample_time = user_sample_time;
-    min_output = user_min_output;
-    max_output = user_max_output;
-    deadband = user_deadband;
-    first_run = true;
-    cumulativeError = 0.0;
+    init_output = 0.0;
+    reverse = false;
+    sample_time = 100;
+    min_output = 0.0;
+    max_output = 255.0;
+    deadband = 0.0;
 
     // Reverse coefficients if in reverse mode
     if (reverse == true) {
@@ -28,20 +37,110 @@ PID::PID(double user_kp, double user_ki, double user_kd, double user_init_output
         ki *= -1.0;
         kd *= -1.0;
     }
+
+    // Initiate other fields
+    first_run = true;
+    cumulative_error = 0.0;
+}
+
+void PID::updateCoeffs(double user_kp, double user_ki, double user_kd) {
+    /*
+     * Updates PID coefficients.
+     * 
+     * @param user_kp: proportional coefficient, should be >= 0
+     * @param user_ki: integral coefficient, should be >= 0
+     * @param user_kd: derivative coefficient, should be >= 0
+     * 
+     * @returns none
+     */
+
+    kp = user_kp;
+    ki = user_ki;
+    kd = user_kd;
+}
+
+void PID::setInitOutput(double user_init_output) {
+    /*
+     * Set initial output
+     *
+     * @param user_init_output: desired initial output, for when we want the output to start at a specific value
+     * 
+     * @returns none
+     */
+}
+
+void PID::setReverse(bool user_reverse) {
+    /*
+     * Toggle reverse mode on or off.
+     *
+     * @param user_reverse: true to turn reverse mode on, false to turn off
+     * 
+     * @returns none
+     */
+
+   if (reverse != user_reverse) {
+        reverse = user_reverse;
+   }
+}
+
+void PID::setSampleTime(unsigned long user_sample_time) {
+    /*
+     * Set sample time (recommended < 100).
+     * 
+     * @param user_sample_time: sample time in ms
+     * 
+     * @returns none
+     */
+
+    sample_time = user_sample_time;
+}
+
+void PID::setOutputBounds(double user_min_output, double user_max_output) {
+    /*
+     * Set output bounds; useful if something like a servo has input limits.
+     *
+     * @param user_min_output: minimum output value (inclusive)
+     * @param user_max_output: maximum output value (inclusive)
+     * 
+     * @returns none
+     */
+
+    min_output = user_min_output;
+    max_output = user_max_output;
+}
+
+void PID::setDeadband(double user_deadband) {
+    /*
+     * Set deadband range (difference between previous and current calculated output, in which the 
+     * previous output is returned) if we need to prevent mechanical wear.
+     * 
+     * @param user_deadband: deadband range, in units of output
+     * 
+     * @returns none
+     */
+
+    deadband = user_deadband;
 }
 
 double PID::compute(double user_setpoint, double user_input) {
+    /*
+     * Computes PID output based on standard PID algorithm; implements deadband functionality and
+     * measures to prevent integral windup. The first run will output the initial output value.
+     * This loop can run for a maximum of 49 days before unsigned long overflows.
+     * 
+     * @param user_setpoint: desired value of something (e.g. servo position)
+     * @param user_input: the acutal value
+     * 
+     * @returns output to be fed back into the controller
+     */
+    
     // If first run, initialize variables
     if (first_run == true) {
         prev_time = millis();
-
-        setpoint = user_setpoint;
-        input = user_input;
-        prev_error = setpoint - input;
-
-        output = init_output;
+        prev_error = user_setpoint - user_input;
         prev_output = init_output;
 
+        output = init_output;
         first_run = false;
     }
 
@@ -56,12 +155,12 @@ double PID::compute(double user_setpoint, double user_input) {
             double error = setpoint - input;
 
             output = init_output + kp * error;
-            output += kd * (error - prev_error) / ((double)time_interval / 1000.0);
+            output += kd * (error - prev_error) / ((double)time_interval * SECS_IN_MS);
 
             // To prevent integral windup, only compute and add integral term if output is not at bounds
-            if (output < max_output  ||  output > min_output) {
-                cumulativeError += error * ((double)time_interval / 1000.0);
-                output += ki * cumulativeError;
+            if (output < max_output  &&  output > min_output) {
+                cumulative_error += error * ((double)time_interval * SECS_IN_MS);
+                output += ki * cumulative_error;
             }
 
             if (output > max_output) {
@@ -86,36 +185,18 @@ double PID::compute(double user_setpoint, double user_input) {
     return output;
 }
 
-void PID::changeSettings(double user_kp, double user_ki, double user_kd, bool user_reverse, unsigned long user_sample_time, double user_min_output, double user_max_output, double user_deadband) {
-    kp = user_kp;
-    ki = user_ki;
-    kd = user_kd;
-    reverse = user_reverse;
-    sample_time = user_sample_time;
-    min_output = user_min_output;
-    max_output = user_max_output;
-    deadband = user_deadband;
-
-    // Reverse coefficients if in reverse mode
-    if (reverse == true) {
-        kp *= -1.0;
-        ki *= -1.0;
-        kd *= -1.0;
-    }
-}
-
-double PID::getSetpoint() {
-    return setpoint;
-}
 double PID::getOutput() {
     return output;
 }
+
 double PID::getkp() {
     return kp;
 }
+
 double PID::getki() {
     return ki;
 }
+
 double PID::getkd() {
     return kd;
 }
--- src/PID_peanat.h
+++ src/PID_peanat.h
@@ -1,21 +1,31 @@
-#ifndef PID_PEANUT_H
-#define PID_PEANUT_H
+#ifndef PID_PEANAT_H
+#define PID_PEANAT_H
+
+/*
+ * This is a PID class to be used in PID control loops. After instantiating an object, we can call the 
+ * public functions to configure settings, send input, and recieve an output. The object is created with
+ * default settings for many parameters.
+ */
 
 class PID {
 
     public:
 
     // Constructor
-    PID(double user_kp, double user_ki, double user_kd, double user_init_output = 0.0, bool user_reverse = false, unsigned long user_sample_time = 100, double user_min_output = 0.0, double user_max_output = 255.0, double user_deadband = 0.0);
+    PID(double user_kp, double user_ki, double user_kd);
+
+    // Change settings
+    void updateCoeffs(double user_kp, double user_ki, double user_kd);
+    void setInitOutput(double user_init_output);
+    void setReverse(bool user_reverse);
+    void setSampleTime(unsigned long user_sample_time);
+    void setOutputBounds(double user_min_output, double user_max_output);
+    void setDeadband(double user_deadband);
 
     // Calculate PID output
     double compute(double user_setpoint, double user_input);
 
-    // Change PID settings (Note: cannot reset init_output)
-    void changeSettings(double user_kp, double user_ki, double user_kd, bool user_reverse = false, unsigned long user_sample_time = 100, double user_min_output = 0.0, double user_max_output = 255.0, double user_deadband = 0.0);
-
     // Get private variables
-    double getSetpoint();
     double getOutput();
     double getkp();
     double getki();
@@ -28,28 +38,28 @@ class PID {
     
     double input;                           // What the value really is
 
-    double output;                          // Output to whatever takes it e.g. servo
+    double output;                          // Calculated output
     double init_output;                     // Output value to start from
-    double prev_output;
+    double prev_output;                     // Previous output
 
-    double kp;                              // PID variables
-    double ki;
-    double kd;
+    double kp;                              // Proportional coefficient
+    double ki;                              // Integral coefficient
+    double kd;                              // Derivative coefficient
 
-    double prev_error;
-    double cumulativeError;                 // For calculating integral output
+    double prev_error;                      // Previous error
+    double cumulative_error;                // Cumulative error for calculating integral output
 
-    unsigned long current_time;             // Time (self explanatory)
-    unsigned long prev_time;
-    unsigned long sample_time;              // Time interval to read in inputs
+    unsigned long current_time;             // Current time in milliseconds
+    unsigned long prev_time;                // Time at which previous input was taken
+    unsigned long sample_time;              // Input sample time
 
-    double min_output;
-    double max_output;
+    double min_output;                      // Max output
+    double max_output;                      // Min output
 
     double deadband;                        // The range in which we don't change the output (to prevent mechanical wear)
     
-    bool reverse;
-    bool first_run;
+    bool reverse;                           // Reverse mode (if true, output * -1)
+    bool first_run;                         // True if it is the very first call to the `compute` function
 };
 
 #endif
